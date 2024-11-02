51365533585a91b171248b835ffdb5ccac36ed02
pkwsr <pakorn166@gmail.com>
2024-11-02 18:16:46 +0700

    xiao small rumen

---
+++ Pages/ComposeMail/composeMail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProject.Pages.ComposeMail
+{
+    public class composeMailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
